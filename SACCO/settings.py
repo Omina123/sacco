2d037afafa54ca41cb4aafe5c519c00d1ec05560
Caleb <Caleb@JUMBE.FIN_SERVER.LOCAL>
2026-04-08 08:46:09 +0300

    update

--- SACCO/settings.py
+++ SACCO/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -86,13 +88,19 @@ WSGI_APPLICATION = 'SACCO.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://sacco_asbe_user:47QnEQuRbzyHoFVT7GphT0GTLdwymsc3@dpg-d7auiscvjg8s73eo1h6g-a.oregon-postgres.render.com/sacco_asbe',
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
